46fc105174c29256e23ef141cb46100415ba511b
Trevor Benoit <Trevorb042@gmail.com>
2024-03-26 12:22:38 -0600

    Complete

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let num3 = 12
+let num4 = 3
+
+let mod = num3 / num4
+
+let numbers = [5, 10, 15, 20, 8];
+let max = Math.max(...numbers);
